7214baab7c03be9c1b02a3867b47b0d1cf62e2de
Nehemy gits <nehemygits@gmail.com>
2024-05-21 17:10:12 +0300

    Second commit

---
+++ assets/js/script.js
@@ -0,0 +1,125 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function () {
+  if (window.scrollY > 300) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeHeader);
+
+
+
+/**
+ * toggle active on add to fav
+ */
+
+const addToFavBtns = document.querySelectorAll("[data-add-to-fav]");
+
+const toggleActive = function () {
+  this.classList.toggle("active");
+}
+
+addEventOnElem(addToFavBtns, "click", toggleActive);
+
+
+
+/**
+ * scroll revreal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
+      sections[i].classList.add("active");
+    } else {
+      sections[i].classList.remove("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Check if MetaMask is installed
+  if (typeof web3 !== 'undefined') {
+   web3 = new Web3(web3.ethereum);
+ } else {
+   // If MetaMask is not installed, provide a message to the user
+   alert('Please install MetaMask to use this application');
+ }
+ 
+ // Check if the user is connected to MetaMask
+ web3.eth.getAccounts()
+   .then(function(accounts) {
+       if (accounts.length === 0) {
+           // The user is not connected to MetaMask
+           alert('Please connect to MetaMask to use this application');
+       } else {
+           // The user is connected to MetaMask
+           var userAddress = accounts[0];
+           alert('Connected to MetaMask. Your Ethereum address is: ' + userAddress);
+       }
+   })
+   .catch(function(error) {
+       console.error(error);
+   });
+ 
+ });
